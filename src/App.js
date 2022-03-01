ba00b6aa7001c9541fd8f9cf49261aabac75e6e8
Jinjong Jeon <56257923+skyfjinjong@users.noreply.github.com>
2022-03-01 16:50:05 +0900

    edit app.js, add Custom.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import './Custom.css';
+import React, { useState } from 'react';
 
 const data = ['apple', 'banana', 'orange'];
 
+function Title({ value1, value2, className, children }) {
+  return (
+    <>
+      <h1>{value1}</h1>
+      <h2>{value2}</h2>
+      <h3 className={className}>{value2}</h3>
+    </>
+  )
+}
+
+function MyButton(props) {
+  return <button style = {{ backgroundColor: "skyblue"}} onClick={props.onMyClick}>
+    {props.value}
+  </button>
+}
+
+function Two({ value }){
+  return <div>투: {value}</div>;
+}
+
+function One({ value }){
+  return (
+    <div>
+      <Two value={value} />
+    </div>
+  )
+}
+
 function App() {
+  function handleClick(str) {
+    return () => {
+      alert(str);
+    }
+  }
+
+  const [count, setCount] = useState(100);
+
+  const [text, setText] = useState('');
+
   return (
     <div className="App">
-      { data.map(v => v) } 입니다.
+      <Title value1 = "제목1입니당" value2 = "제목2입니당" className="box"/>
+
+      <MyButton value="버튼입니당" onMyClick={() => alert("hi")} />
+      <MyButton value="버튼입니당2" onMyClick={() => alert("bye")} />
+
+      <One value="omg~"/>
+
+      <div className = "box">Hello</div>
+
+      <button onClick={handleClick("Hello~!")}>Click me</button>
+      
+      { data.map((v) => (
+        <p>{v}</p>
+      ))}
+
+      <button
+        onClick ={() => {
+          setCount(count + 1);
+        }}
+        >
+          Counter
+        </button>
+        <p>{count}</p>
+      
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+      <p>입력된 값 : {text}</p>
     </div>
   );
 }
